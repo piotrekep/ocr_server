339fab272bcf9fefd18dbee60fed2fdd569bf7ad
piotrek <piotrr.markowski@gmail.com>
2025-03-03 00:01:15 +0100

    zmiana typu bufora z vector na list, i zachowanie ze "stack" do fifo

--- include/datastore.hpp
+++ include/datastore.hpp
@@ -2,71 +2,59 @@
 #define DATASTORE_HPP
 
 #include <string>
-#include <unordered_map>
+#include <list>
 #include <mutex>
+#include <utility>
 #include <opencv2/opencv.hpp>
-
 
 template <typename T>
 class DataStore {
 public:
     
     int store(const T& input) {
-    std::pair<int,  T> data;
-    data.first=id;
-    data.second=input;
-    std::lock_guard<std::mutex> lock(mutex_);
-    data_.push_back(data);
-    id++;
-    return id;
+        std::pair<int, T> data;
+        data.first = id;
+        data.second = input;
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            data_.push_back(data);
+        }
+        id++;
+        return id;
     }
 
-    void store(int id,const T& input) {
-    std::pair<int, T> data;
-    data.first=id;
-    data.second=input;
-    std::lock_guard<std::mutex> lock(mutex_);
-    data_.push_back(data);
+    
+    void store(int id, const T& input) {
+        std::pair<int, T> data;
+        data.first = id;
+        data.second = input;
+        std::lock_guard<std::mutex> lock(mutex_);
+        data_.push_back(data);
     }
 
+    
     std::pair<int, T> get(){
-    std::lock_guard<std::mutex> lock(mutex_);
-    if(data_.empty()){
-
-        return std::make_pair(-1, cv::Mat());
+        std::lock_guard<std::mutex> lock(mutex_);
+        if(data_.empty()){
+            
+            return std::make_pair(-1, cv::Mat());
+        }
+        
+        auto element = data_.front();
+        data_.pop_front();   
+        return element;
     }
-    auto element = data_.back();  // Retrieve the last element
-    data_.pop_back();   
-    return element;
-}
 
-   
+    
     bool checkEmpty(void){
-    if(data_.empty()) return true;
-    else return false;
- }
+        std::lock_guard<std::mutex> lock(mutex_);
+        return data_.empty();
+    }
 
 private:
     inline static int id = 0;
-    std::vector<std::pair<int, T>> data_;
+    std::list<std::pair<int, T>> data_;
     mutable std::mutex mutex_;
 };
 
-/*
-class DataStore {
-public:
-    
-    int store(const cv::Mat& img);
-
-    std::pair<int, cv::Mat>get(void);
-    
-    bool checkEmpty(void);
-
-private:
-    inline static int id=0;
-    std::vector<std::pair<int, cv::Mat>> data_;
-    mutable std::mutex mutex_; 
-};
-
-*/
 #endif 
